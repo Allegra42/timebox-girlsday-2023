3c1dad15986d52e49b0d2a11805cfaba53e07e84
Phuong Mai <pmai@inovex.de>
2023-04-18 12:59:17 +0000

    Merge adaptions into main

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from machine import *
 from time import *
 from lib.helper import *
 
-
 # Teil 1: LEDs
 
 # Aufgabe 1: Farben definieren
@@ -21,19 +20,19 @@ from lib.helper import *
 # Beispiel:
 red = (255, 0, 0)
 lila = (10, 0, 10)  # Lila setzt sich aus gleichen Anteilen von rot und blau zusammen.
-#
-# Definiere hier weitere Farben!
+
+# TODO (Aufgabe 1): Definiere hier weitere Farben!
 
 
 # Aufgabe 2: Farben anzeigen
 #
-# Damit die Farben auch auf dem Ring angezeigt werden, schreiben wir eine Funktion
+# Damit die Farben auch auf dem Ring angezeigt werden, schreiben wir eine Funktion,
 # die uns dabei hilft.
-# Der Ring hat 12 LEDs die unabhängig von einander gesteuert werden können.
-# Dazu muss angegeben werden welche LED in welcher Farbe leuchten soll.
-# Der Code dazu sagt aus setze das Pixel an der Stelle [Nummer] auf den Wert der Farbe.
+# Der Ring hat 12 LEDs, die unabhängig von einander gesteuert werden können.
+# Dazu muss angegeben werden, welche LED in welcher Farbe leuchten soll.
+# Der Code dazu sagt aus, setze das Pixel an der Stelle [Nummer] auf den Wert der Farbe.
 # number und color sind dabei Funktionsargumente. Wenn wir die Funktion später
-# nutzen um die LEDs leuchten zu lassen schreiben wir zum Beispiel:
+# nutzen, um die LEDs leuchten zu lassen, schreiben wir zum Beispiel:
 #
 # set_pixel(0, red)
 #
@@ -42,13 +41,31 @@ lila = (10, 0, 10)  # Lila setzt sich aus gleichen Anteilen von rot und blau zus
 # Ergänze die fehlende Anweisung!
 def set_pixel(number, color):
     pixel[number] = color
-    #pixel.write()
+    # pixel.write() # TODO (Aufgabe 2): Kommentiere die Zeile aus, um die LEDs auch leuchten zu lassen
+
 
 # Testzeit!
 #
-# Suche weiter unten in der Datei die Funktion setup() und rufe von dort
-# set_pixel(number, color) für verschiedene Pixel im Ring und deine
-# vorher definierten Farben auf.
+# Rufe nun set_pixel(number, color) für verschiedene Pixel im Ring und deine
+# vorher definierten Farben in der Methode setup() auf.
+
+# Diese Funktion wird exakt einmal beim Start des Mikrocontollers ausgeführt.
+# Hier steht alles, was zu Beginn eingerichtet werden muss.
+def setup():
+    global rtc
+    global pixel
+
+    rtc = RTC()
+    pin = Pin(23, Pin.OUT)
+    pixel = NeoPixel(pin, 12)
+
+    connect_to_wlan("inovexgast", "in0vexGast")
+    set_exact_time(rtc)
+
+    # TODO (Aufgabe 2): Lass deine Farbe aufleuchten
+    #  - Nutze die Funktion set_pixel(number, color) hier, um LEDs direkt beim
+    #    Start des Mikrocontrollers leuchten zu lassen!
+    #  - Teste den Code
 
 
 # Aufgabe 3: LEDs ausschalten
@@ -60,18 +77,18 @@ def set_pixel(number, color):
 # Aber Informatiker sind ziemlich faul und schreiben nicht gerne so viel ähnlichen Code.
 # Wir nutzen dazu Schleifen!
 # For-Schleifen erledigen einen Ablauf genau so oft wie gewünscht.
-# Die Zählvariable i sagt im wie vielten Durchlauf der Schleife wir uns befinden.
+# Die Zählvariable i gibt an, im wie vielten Durchlauf der Schleife wir uns befinden.
 #
 # Beispiel:
 # for i in range(5):
 #     # tut etwas
 #
-# Führt alles was eingerückt bei "tut etwas" steht genau 5 mal aus.
+# Die obige Schleife führt alles, was eingerückt bei "tut etwas" steht, genau 5 mal aus.
 #
 # Schreibe den fehlenden Code, damit alle 12 LEDs ausgeschaltet werden!
 def clear_pixels():
-    for i in range( ): # 12
-        pixel[i] = # (0, 0, 0) als Farbdefinition
+    for i in range():  # TODO (Aufgabe 3): Lasse die Schleife 12 mal ausführen
+        pixel[i] = ()  # TODO (Aufgabe 3): Setze das Tupel (0, 0, 0) als Farbdefinition
 
     pixel.write()
 
@@ -83,19 +100,22 @@ def clear_pixels():
 # Farben leuchten und blinken zu lassen!
 
 
-
 # Teil 2: Uhr
 #
-# Aufgabe 3:
+# Aufgabe 4:
 #
-# Mit dem was wir über den LED Ring gelernt haben lassen sich nun praktische Dinge bauen - zum Beispiel eine Uhr.
+# Mit dem was wir über den LED Ring gelernt haben lassen sich nun praktische Dinge
+# bauen - zum Beispiel eine Uhr.
 #
 # Der Mikrocontroller ESP32 hat sowohl WLAN als auch eine sogenannte Echtzeituhr.
-# Über WLAN kann der ESP32 initial einmal bei einer sehr genauen Zeitquelle im Internet die exakte Uhrzeit (und das Datum) erfragen. 
-# Durch die Echtzeituhr ist er in der Lage die Zeit genau weiter zu zählen, selbst wenn für einige Zeit nicht die Zeitquelle nicht verfügbar sein sollte.
+# Über WLAN kann der ESP32 initial einmal bei einer sehr genauen Zeitquelle im
+# Internet die exakte Uhrzeit (und das Datum) erfragen.
+# Durch die Echtzeituhr ist er in der Lage die Zeit genau weiter zu zählen, selbst
+# wenn für einige Zeit nicht die Zeitquelle nicht verfügbar sein sollte.
 # Perfekt für unsere eigene Uhr!
 #
-# Um die exakte Zeit zu erfragen und der Echtzeituhr mitzuteilen musst du nichts weiter tun - Das wird schon in der setup-Funktion 
+# Um die exakte Zeit zu erfragen und der Echtzeituhr mitzuteilen, musst du nichts
+# weiter tun - Das wird schon in der setup-Funktion
 # durch den Aufruf von set_exact_time(rtc) erledigt.
 #
 #
@@ -106,7 +126,9 @@ def clear_pixels():
 #
 #       current_time = rtc.datetime()
 #
-# in die Loop-Funktion, um die aktuelle Uhrzeit in jedem Schleifendurchlauf in die Variable current_time (du kannst sie auch anders nennen wenn das einfacher für dich ist) zu speichern.
+# in die Loop-Funktion, um die aktuelle Uhrzeit in jedem Schleifendurchlauf in die
+# Variable current_time (du kannst sie auch anders nennen, wenn das einfacher für dich ist)
+# zu speichern.
 # Damit du nun auch etwas siehst, schreibe
 #
 #       print(current_time)
@@ -117,42 +139,47 @@ def clear_pixels():
 # Lösung: (year, month, day, weekday, hours, minutes, seconds, subseconds)
 
 
-# Aufgabe 4:
+# Aufgabe 5:
 #
-# Nun wollen wir die Zeit auf dem Pixelring darstellen - überlege welche Möglichkeiten es mit den 12 Pixeln dafür gibt.
-# Wenn deine Idee von der Lösung hier abweicht kannst du später noch eine eigene Variante bauen oder diese hier verfeinern!
+# Nun wollen wir die Zeit auf dem Pixelring darstellen - überlege, welche Möglichkeiten
+# es mit den 12 Pixeln dafür gibt.
+# Wenn deine Idee von der Lösung hier abweicht, kannst du später noch eine eigene
+# Variante bauen oder diese hier verfeinern!
 # Deine Kreativität hat hier keine Grenzen.
 #
 #
 # Eine erste und einfache Idee ist es, sich an einer klassischen Uhr zu orientieren.
 # Auf der sind ebenfalls die Zahlen von 1 - 12 (also passend zu unseren 12 Pixeln) dargestellt.
 # Stunden, Minuten und Sekundenzeiger können zum Beispiel durch verschiedene Farben realisiert werden.
-# Was soll passieren wenn die Zeiger übereinander stehen?
+# Was soll passieren, wenn die Zeiger übereinander stehen?
 # Wie könnte man damit umgehen, dass im Gegensatz zu einer normalen Uhr keine "Minutenpixel" verfügbar sind?
 #
 #  1. Stunde setzen
-# 
+#
 # Eigentlich ist es ganz einfach die Stunde anzuzeigen. Genau wie bei einer normalen Uhr.
 # Wir haben 12 Pixel für 2 mal 12 Stunden - Vormittags und Nachmittags, am und pm, wie auch im Englischen.
 # Die Stunde wird aber im 24h Format dargestellt.
 # Also was ist zu tun?
 #
-# WENN die Stunde > 11 ist 
+# WENN die Stunde > 11 ist
 #   ist die (neue) Stunde = die (alte) Stunde - 12
 #
 # Dann musst du nur noch für die Stunde das entsprechende Pixel setzen.
-# Das geht wie zuvor, nur dass du als Index welches Pixel du anschalten willst jetzt einfach die Variable hour nutzen kannst.
+# Das geht wie zuvor, nur dass du als Index, welches Pixel du anschalten willst, jetzt einfach die
+# Variable hour nutzen kannst.
 #
-# Such dir eine Farbe aus und schreibe die Logik für die Stunde als Programmcode in die Funktion set_time(hour, minute, second)!
+# Such dir eine Farbe aus und schreibe die Logik für die Stunde als Programmcode in die Funktion
+# set_time(hour, minute, second)!
 #
-# 
+#
 # Testzeit!
 #
 # Rufe die Funktion set_time(hour, minute, second) in der Loop-Funktion auf!
 # Dazu musst du dich kurz an die letzte Aufgabe erinnern.
-# Wir hatten die aktuelle Uhrzeit in der Variablen current_time gespeichert und du hast dir überlegt welche der ausgegebenen Zahlen was bedeutet.
+# Wir hatten die aktuelle Uhrzeit in der Variablen current_time gespeichert und du hast dir überlegt,
+# welche der ausgegebenen Zahlen was bedeutet.
 # Aktuell interessieren wir uns nur für die Stunde, die Minute und die Sekunde.
-# Einen einzelnen Wert aus dem gesamten Gebilde current_time heraus zu bekommen können wir einfach schreiben
+# Einen einzelnen Wert aus dem gesamten Gebilde current_time können wir durch folgende Zeile erhalten
 #
 #   wert = current_time[index]
 #
@@ -165,78 +192,65 @@ def clear_pixels():
 
 
 def set_time(hour, minute, second):
-    if hour > 11 :
-        hour = hour - 12
-    hour = hour + 1     # fix time zone
+    # TODO (Aufgabe 5): Schreibe die Logik für die Stunde, indem du 24 Stunden mittels 12 Pixel darstellst
+    # if hour > 11:
+    #     hour = hour - 12
+    # TODO (Aufgabe 5, optional): Passe die Zeitzone mit der Variable hour an
+    # hour = hour + 2  # fix time zone
 
-    minute = int (minute / 5)
-    second = int (second / 5)
+    minute = int(minute / 5)
+    second = int(second / 5)
     print("hour:")
     print(hour)
     print("minute:")
     print(minute)
 
-    pixel[hour] = (10, 0, 0)
+    # TODO (Aufgabe 5): Setze den Pixel für die Stundenangabe analog zu der Minute und der Sekunde
+    # pixel[hour] = (10, 0, 0)
+    # TODO (Aufgabe 5): Teste den Code nun weiter unten in loop()
+
     pixel[minute] = (0, 10, 0)
     pixel[second] = (0, 0, 10)
-    
+
     if second == minute:
         pixel[minute] = (0, 10, 10)
-    
+
     if second == hour:
         pixel[hour] = (10, 0, 10)
-    
+
     if minute == hour:
         pixel[hour] = (10, 10, 0)
         if second == minute:
             pixel[hour] = (10, 10, 10)
-    
+
     pixel.write()
 
 
-
-# Diese Funktion wird exakt einmal beim Start des Mikrocontollers ausgeführt.
-# Hier steht alles, was zu Beginn eingerichtet werden muss.
-def setup():
-    global rtc
-    global pixel
-    
-    rtc = RTC()
-    pin = Pin(23, Pin.OUT)
-    pixel = NeoPixel(pin, 12)
-
-    connect_to_wlan("inovexgast", "in0vexGast")
-    set_exact_time(rtc)
-    
-    # Testzeit 1:
-    # Nutze die Funktion set_pixel(number, color) hier um LEDs direkt beim
-    # Start des Mikrocontrollers leuchten zu lassen!
-    
-    
-#     
 def loop():
     while True:
-        
-        # Testzeit 2:
-        # Nutze set_pixel(number, color) und clear_pixels() auf, um die LEDs in verschiedenen
-        # Farben leuchten und blinken zu lassen!
-        #
-        # Achtung:
-        # Die Funktion loop() wird in einer ewigen Schleife ausgeführt!
-        # Um einen Effekt zu sehen, muss zwischen den einzelnen Anweisungen etwas Zeit vergehen.
-        # Nutze sleep(5) um das An- und Ausschalten zu verzögern!
-        # Was passiert wenn du sleep(10) schreibst?
-        
-        
-        #temperature = (esp32.raw_temperature() - 32) / 1.8
-        #current_time = rtc.datetime()
-        #print(temperature)
-        #print(current_time)
-        #clear_pixels()
-        #set_time(current_time[4], current_time[5], current_time[6])
-        #sleep(3)
+        # TODO (Aufgabe 3): Lasse die LEDs in verschiedenen Farben leuchten
+        #  - Rufe dafür set_pixel(number, color) und clear_pixels() auf, um die LEDs in verschiedenen
+        #    Farben leuchten und blinken zu lassen!
+        #  - Teste den Code
+        #  Achtung:
+        #  Die Funktion loop() wird in einer ewigen Schleife ausgeführt!
+        #  Um einen Effekt zu sehen, muss zwischen den einzelnen Anweisungen etwas Zeit vergehen.
+        #  Nutze sleep(3) um das An- und Ausschalten zu verzögern!
+        #  Was passiert, wenn du sleep(10) schreibst?
 
+        # TODO (Aufgabe 4): Frage die Uhrzeit ab
+        #  - Speichere die jetzige Uhrzeit in einer Variable namens current_time
+        #  - Schreibe die Variable ins Log aus
+        # current_time = rtc.datetime()
+        # print(current_time)
 
+        # TODO (Aufgabe 5): Setze die Zeit
+        #  - Setze die Zeit mit der jetzigen Uhrzeit, die du vorher gespeichert hast
+        #  - Teste den Code
+        # clear_pixels()
+        # set_time(current_time[4], current_time[5], current_time[6])
+        print("schlafe")  # TODO: Lösche die Zeile nach dem Testen
+        sleep(3)
 
 
 # Hier brauchst du nichts machen,
@@ -246,5 +260,5 @@ def main():
     loop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
